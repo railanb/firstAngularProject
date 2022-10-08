d1998be269e24d6b29d0b476ee3c511f1fbc1ee9
Railan Brandão <railanb.rb@hotmail.com>
2022-10-08 20:02:35 -0300

    Envio do projeto introdutorio para repositorio

---
+++ src/app/tasks/shared/task.service.ts
@@ -0,0 +1,51 @@
+import { Task } from './task';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+  tasks: Task[] = [
+    {id: 1, description: 'Tarefa 1', completed: false},
+    {id: 2, description: 'Tarefa 2', completed: false},
+    {id: 3, description: 'Tarefa 3', completed: true},
+    {id: 4, description: 'Tarefa 4', completed: false},
+    {id: 5, description: 'Tarefa 5', completed: false},
+    {id: 6, description: 'Tarefa 6', completed: false},
+    {id: 7, description: 'Tarefa 7', completed: false},
+    {id: 8, description: 'Tarefa 8', completed: false},
+    {id: 9, description: 'Tarefa 9', completed: false},
+    {id: 10, description: 'Tarefa 10', completed: false},
+    {id: 11, description: 'Tarefa 11', completed: false},
+    {id: 12, description: 'Tarefa 12', completed: false},
+  ]
+
+  constructor() { }
+
+  getAll() {
+    return this.tasks;
+  }
+
+  getById(id: number){
+    const task = this.tasks.find((value) => value.id == id);
+    return task;
+  }
+
+  save(task: Task) {
+    if (task.id) {
+      const taskArr = this.getById(task.id);
+      taskArr.description = task.description;
+      taskArr.completed = task.completed;
+    } else {
+      const lastId = this.tasks[this.tasks.length-1].id;
+      task.id = lastId + 1;
+      task.completed = false;
+      this.tasks.push(task);
+    }
+  }
+
+  delete(id: number) {
+    const taskIndex = this.tasks.findIndex((value) => value.id == id);
+    this.tasks.splice(taskIndex, 1);
+  }
+}
